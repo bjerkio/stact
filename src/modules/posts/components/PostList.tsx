7e39a5cb169f712cf1af30038aab0f5eb6bf8a8f
Roger Andersen <ra@bjerk.io>
2019-09-17 10:30:45 +0200

    fix(PostList): items is not guaranteed to not be null

--- src/modules/posts/components/PostList.tsx
+++ src/modules/posts/components/PostList.tsx
@@ -7,9 +7,10 @@ type Props = { posts: PostCollection };
 
 const PostList: React.FC<Props> = ({ posts }) => (
   <>
-    {posts.items!.map(post => post && <Post key={post.id} {...post} />)}
+    {posts.items &&
+      posts.items.map(post => post && <Post key={post.id} {...post} />)}
   </>
-)
+);
 
 export default connect(
   (state: { posts: { posts: PostCollection } }) => ({
