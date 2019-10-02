a3296975d293091e56c7bd37d22b71106651bba4
Roger Andersen <ra@bjerk.io>
2019-10-02 12:20:19 +0200

    fix: run createEpicMiddleware for each store instance

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,11 @@ import { createEpicMiddleware } from 'redux-observable';
 import reducers from './reducers';
 import epics from './epics';
 
-const epicMiddleware = createEpicMiddleware();
-
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
 
 export default function configureStore(): Store {
+  const epicMiddleware = createEpicMiddleware();
   const store = createStore(
     reducers,
     composeEnhancers(applyMiddleware(epicMiddleware)),
